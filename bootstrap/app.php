aac635a0820e4bf134b02362c7b292ba42811c70
NkGitDev <nkjoshidev@gmail.com>
2026-07-25 14:50:09 +0530

    Trust proxy for Livewire uploads on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Render.com ke reverse proxy ko trust karne ke liye
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
